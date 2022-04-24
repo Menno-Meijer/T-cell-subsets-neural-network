ac529ad6e66cb937f7dee9c367c1a1a7c82f2d72
Menno <mennomeijer01@gmail.com>
2022-04-24 19:32:02 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras.callbacks import EarlyStopping
+
+np.random.seed(5)
+
+## Model:
+model = keras.Sequential([
+    keras.layers.BatchNormalization(input_shape=[12],),
+    keras.layers.Dense(256,   activation="relu"),
+    keras.layers.BatchNormalization(),
+
+    keras.layers.Dense(256,  activation="relu"),
+    keras.layers.BatchNormalization(),
+
+    keras.layers.Dense(256,  activation="relu"),
+    keras.layers.BatchNormalization(),
+
+    keras.layers.Dense(256, activation="relu"),
+    keras.layers.BatchNormalization(),
+
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(
+            optimizer = keras.optimizers.Adam(learning_rate=0.001),
+            loss="sparse_categorical_crossentropy",
+            # metrics=['accuracy', 'binary_accuracy', 'categorical_accuracy']
+            metrics = ["accuracy"]
+)
+# train data preparation
+train_df = pd.read_csv("data/train_total.csv")
+train_x = np.column_stack((train_df.FSC_A.values,
+                           train_df.FSC_H.values,
+                           train_df.FSC_W.values,
+                           train_df.SSC_A.values,
+                           train_df.SSC_H.values,
+                           train_df.SSC_W.values,
+                           train_df.CD4.values,
+                           train_df.CD5.values,
+                           train_df.CD8.values,
+                           train_df.LIVE_DEAD.values,
+                           train_df.CD197.values,
+                           train_df.CD45RA.values,
+                           ))
+
+one_hot_class_table = pd.get_dummies(train_df["class"])
+    # Classes:
+    # CD4_tcm - CD4_tem - CD4_temra - CD4_th0 - CD8_tc0 - CD8_tcm - CD8_tem - CD8_temra - debris - none_live
+    # Number the cell type (train_y) to make the model and prediction simpler:
+class_dict = {}
+for i, cell_class in enumerate(one_hot_class_table.columns):
+    class_dict[cell_class] = i
+print(class_dict)
+
+train_y = train_df["class"].apply(lambda x: class_dict[x])
+
+
+# test data preparation:
+test_df = pd.read_csv("data/test_total.csv")
+test_x = np.column_stack((test_df.FSC_A.values,
+                     test_df.FSC_H.values,
+                     test_df.FSC_W.values,
+                     test_df.SSC_A.values,
+                     test_df.SSC_H.values,
+                     test_df.SSC_W.values,
+                     test_df.CD4.values,
+                     test_df.CD5.values,
+                     test_df.CD8.values,
+                     test_df.LIVE_DEAD.values,
+                     test_df.CD197.values,
+                     test_df.CD45RA.values,
+                     ))
+
+    # Number the cell types (test_y)
+test_y = test_df["class"].apply(lambda x: class_dict[x])
+
+
+# Callback to stop the model early and fit
+callback = EarlyStopping(monitor="val_loss",
+                         mode="min",
+                         verbose=1,
+                         patience=2)
+
+model.fit(train_x, train_y,
+          batch_size=8000,
+          epochs=10,
+          verbose=2,
+          validation_data =(test_x, test_y),
+          callbacks = [callback]
+          )
+
+# Save the model:
+model.save("FlowCytometry_model")
---
+++ predict.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+
+from tensorflow import keras
+
+np.random.seed(5)
+
+#Load in test dataset:
+test_df = pd.read_csv("data/test_total.csv")
+test_x = np.column_stack((test_df.FSC_A.values,
+                     test_df.FSC_H.values,
+                     test_df.FSC_W.values,
+                     test_df.SSC_A.values,
+                     test_df.SSC_H.values,
+                     test_df.SSC_W.values,
+                     test_df.CD4.values,
+                     test_df.CD5.values,
+                     test_df.CD8.values,
+                     test_df.LIVE_DEAD.values,
+                     test_df.CD197.values,
+                     test_df.CD45RA.values,
+                     ))
+
+
+##### Load model #####
+model = keras.models.load_model("FlowCytometry_model", compile = True)
+
+one_hot_class_table = pd.get_dummies(test_df["class"])
+
+# classes:
+# CD4_tcm - CD4_tem - CD4_temra - CD4_th0 - CD8_tc0 - CD8_tcm - CD8_tem - CD8_temra - debris - none_live
+
+# Add the different classes to a dict
+class_dict = {}
+for i, x in enumerate(one_hot_class_table.columns):
+    class_dict[x] = i
+test_y = test_df["class"].apply(lambda x: class_dict[x])
+
+#validation
+print("Evaluate")
+model.evaluate(test_x, test_y, verbose=2)
+
+reverse_class_dict = dict(enumerate(one_hot_class_table.columns))
+
+##### Predict #####
+
+predict = pd.Series(np.round(model.predict(test_x)).argmax(1))
+
+predict_class = pd.Series(predict.apply(lambda x: reverse_class_dict[x]))
+test_df["predict_class"] = predict_class
+false_predict = test_df[test_df["class"] != test_df["predict_class"]]
+
+print(f"Accuracy = {100-((len(false_predict)/len(test_df))*100)}%")
+test_df.to_csv("data/test_predicted.csv", index=False)
+
+#Accuracy on total set: 99.04%
+#Accuracy on test set: 99.05%
+
+
+
+
+
